a75514bf6d2c827c6fdc7249cd43f86abf6444eb
thr3a <thr3a@users.noreply.github.com>
2022-10-02 18:58:39 +0000

    deploy: d8b0c92840e0d0188738498435fa09b1711267b8

---
+++ _next/static/GwXs_zW7kg6Hbtb_xA3Zp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/647-3a2683ec57d9ad85.js","static/chunks/pages/index-8d30ee556a60bb51.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/wwTBFcz0tS_ub49frxe6f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/260-05a7b2ee65dd93ca.js","static/chunks/pages/index-355849174d0d8a6f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
